78b2eace36883f45bc43601cc2a56b7403a3c1c5
Joey Lee <josephkanglee@gmail.com>
2016-01-10 17:09:53 -0800

    added map box js and slider line control

--- js/app.js
+++ js/app.js
@@ -1,190 +1,235 @@
 $(document).ready(function() {
 
-    var map = L.map('map',{zoom: 17, center: new L.latLng([49.23,-123.15]) });
+    // var map = L.map('map', {
+    //     zoom: 17,
+    //     center: new L.latLng([49.23, -123.15])
+    // });
 
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    // }).addTo(map);
 
-	// var MapBox = L.tileLayer('http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-	// 	attribution: 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-	// 	subdomains: 'abcd',
-	// 	id: 'joeyklee',
-	// 	accessToken: 'pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw'
-	// }).addTo(map);
+    // L.mapbox.accessToken = 'pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw';
+    // var map = L.mapbox.map('map', 'mapbox.streets',{
+    //     zoomControl: false
+    // }).setView([49.26, -123.10], 17);
 
-
-
-
-    
+    L.mapbox.accessToken = 'pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw';
+    var map = L.mapbox.map('map', 'mapbox.streets',{
+        zoomControl: false
+    }).setView([49.26, -123.10], 13);
 
 
-    // (function init() {
+    // var MapBox = L.tileLayer('http://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    //  attribution: 'Imagery from <a href="http://mapbox.com/about/maps/">MapBox</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    //  subdomains: 'abcd',
+    //  id: 'joeyklee',
+    //  accessToken: 'pk.eyJ1Ijoiam9leWtsZWUiLCJhIjoiMlRDV2lCSSJ9.ZmGAJU54Pa-z8KvwoVXVBw'
+    // }).addTo(map);
 
-    		// var marker = L.circle([0, 0], 10).addTo(map);
 
-    		// function update() {
-    		//     map.locate({
-    		//         setView: true,
-    		//         maxZoom: 16
-    		//     });
-    		//     function onLocationFound(e) {
-    		//         // console.log(e.latlng);
-    		//         var radius = e.accuracy / 2;
-    		//         marker.setLatLng(e.latlng, radius);
-    		//     }
-    		//     map.on('locationfound', onLocationFound);
-    		// }
-    		// setInterval(update, 100);
 
-            map.addControl( new L.Control.Gps({autoActive:true, autoCenter: true, style: {              //default L.CircleMarker styles
+    map.addControl(new L.Control.Gps({
+        autoActive: true,
+        autoCenter: true,
+        style: { //default L.CircleMarker styles
             radius: 15,
             weight: 2,
             color: '#c20',
             opacity: 1,
             fillColor: '#f23',
             fillOpacity: 0.75
-        },}) );//inizialize control
+        },
+    })); //inizialize control
 
+    var studyarea = 'data/studyarea.geojson';
+    d3.json(studyarea, function(data) {
+        console.log(data);
 
-      
+        function style(feature) {
+            return {
+                weight: 4,
+                color: "#FF3300",
+                opacity: 0.5,
+                fillOpacity: 0
+            };
+        };
 
-            var studyarea = 'data/studyarea.geojson';
-            d3.json(studyarea, function(data) {
+        L.geoJson(data, {
+            style: style
+        }).addTo(map);
+
+    }); // end d3
+
+    $(function() {
+
+        d3.json('data/1641_1.geojson', function(err, data){
                 console.log(data);
 
-                function style(feature) {
-                    return {
-                        weight: 4,
-                        color: "#FF3300",
-                        opacity: 0.5,
-                        fillOpacity: 0
-                    };
-                };
+                var coords = [];
 
-                L.geoJson(data, {
-                    style: style
-                }).addTo(map);
-
-            }); // end d3
-
-
-            var way_1641 = L.layerGroup();
-            var way_0205 = L.layerGroup();
-            var way_0150 = L.layerGroup();
-            var way_0151 = L.layerGroup();
-            var way_0108 = L.layerGroup();
-
-            var ifile = {
-                "1641": 'data/1641_1.geojson',
-                "0205": 'data/0205_1.geojson',
-                "0150": 'data/0150_1.geojson',
-                "0151": 'data/0151_1.geojson',
-                "0108": 'data/0108_1.geojson'
-            };
-            var waycolor = '#3366FF';
-
-            loadRoute(ifile["1641"], '#7519FF', way_1641);
-            loadRoute(ifile["0205"], '#CC33FF', way_0205);
-            loadRoute(ifile["0150"], '#006600', way_0150);
-            loadRoute(ifile["0151"], '#003366', way_0151);
-            loadRouteMulti(ifile["0108"], '#FF0000', way_0108);
-
-            var baseMaps = {
-                "1641": way_1641,
-                "0205": way_0205,
-                "0150": way_0150,
-                "0151": way_0151,
-                "0108": way_0108
-            };
-
-            L.control.layers(baseMaps, null).addTo(map);
-
-            function loadRoute(ifile, waycolor, layerobject) {
-                d3.json(ifile, function(data) {
-                    // console.log(data.features[0].geometry.coordinates);
-                    var coords = data.features[0].geometry.coordinates;
-
-                    var newcoords = [];
-                    coords.forEach(function(d) {
-                        var temp = [d[1], d[0]];
-                        newcoords.push(temp);
-                    });
-
-                    var pathPattern = L.polylineDecorator(newcoords, {
-                        patterns: [{
-                            offset: 0,
-                            repeat: 10,
-                            symbol: L.Symbol.dash({
-                                pixelSize: 5,
-                                pathOptions: {
-                                    color: waycolor,
-                                    weight: 3,
-                                    opacity: 0.75
-                                }
-                            })
-                        }, {
-                            offset: 25,
-                            repeat: 100,
-                            symbol: L.Symbol.arrowHead({
-                                pixelSize: 10,
-                                pathOptions: {
-                                    color: waycolor,
-                                    fillOpacity: 1,
-                                    weight: 0
-                                }
-                            })
-                        }]
-                    }).addTo(layerobject);
-                }); // end d3
-            };
-
-            function loadRouteMulti(ifile, waycolor, layerobject) {
-                d3.json(ifile, function(data) {
-                    // console.log(data.features[0].geometry.coordinates);
-                    var coords = data.features[0].geometry.coordinates;
-
-                    var newcoords = [];
-                    coords.forEach(function(d) {
-                        // console.log(d)
-                        d.forEach(function(e) {
-                            // console.log(e);
-                            var temp = [e[1], e[0]];
-                            newcoords.push(temp);
-                        })
+                data.features.forEach(function(val){
+                    // console.log(val.geometry.coordinates);
+                    val.geometry.coordinates.forEach(function(d){
+                        // console.log(d);
+                        coords.push([d[1], d[0]]);
                     })
 
-                    var pathPattern = L.polylineDecorator(newcoords, {
-                        patterns: [{
-                            offset: 0,
-                            repeat: 10,
-                            symbol: L.Symbol.dash({
-                                pixelSize: 5,
-                                pathOptions: {
-                                    color: waycolor,
-                                    weight: 3,
-                                    opacity: 0.75
-                                }
-                            })
-                        }, {
-                            offset: 25,
-                            repeat: 100,
-                            symbol: L.Symbol.arrowHead({
-                                pixelSize: 10,
-                                pathOptions: {
-                                    color: waycolor,
-                                    fillOpacity: 1,
-                                    weight: 0
-                                }
-                            })
-                        }]
-                    }).addTo(layerobject);
-                }); // end d3
-            };
+                    // val.geometry.coordinates.forEach(function(d){
+                    //     // console.log(d);
+                    //     // coords.push([d[1], d[0]]);
+
+                    //     d.forEach(function(item){
+                    //         coords.push([item[1], item[0]]);
+
+                    //         // console.log(item);
+                    //     })
+                    // })
+                })
+
+                // console.log( coords.slice(75, 200));
+
+                var polyline = L.polyline(coords, {null: 'red'}).addTo(map);
+            
+
+        $( "#slider-range" ).slider({
+          range: true,
+          min: 0,
+          max: coords.length,
+          values: [ 0, coords.length ],
+          slide: function( event, ui ) {
+            $( "#amount" ).val( "#" + ui.values[ 0 ] + " - #" + ui.values[ 1 ] );
+
+            polyline.setLatLngs(coords.slice(ui.values[0], ui.values[1]));
+
+          }
+        });
+
+
+        $( "#amount" ).val( "#" + $( "#slider-range" ).slider( "values", 0 ) +
+          " - #" + $( "#slider-range" ).slider( "values", 1 ) );
+
+        })
+      });
+
+
+    var way_1641 = L.layerGroup();
+    var way_0205 = L.layerGroup();
+    var way_0150 = L.layerGroup();
+    var way_0151 = L.layerGroup();
+    var way_0108 = L.layerGroup();
+
+    var ifile = {
+        "1641": 'data/1641_1.geojson',
+        "0205": 'data/0205_1.geojson',
+        "0150": 'data/0150_1.geojson',
+        "0151": 'data/0151_1.geojson',
+        "0108": 'data/0108_1.geojson'
+    };
+    var waycolor = '#3366FF';
+   
+
+    loadRoute(ifile["1641"], '#7519FF', way_1641);
+    loadRoute(ifile["0205"], '#CC33FF', way_0205);
+    loadRoute(ifile["0150"], '#006600', way_0150);
+    loadRoute(ifile["0151"], '#003366', way_0151);
+    loadRouteMulti(ifile["0108"], '#FF0000', way_0108);
+
+    var baseMaps = {
+        "1641": way_1641,
+        "0205": way_0205,
+        "0150": way_0150,
+        "0151": way_0151,
+        "0108": way_0108
+    };
+
+    L.control.layers(baseMaps, null).addTo(map);
+
+    function loadRoute(ifile, waycolor, layerobject) {
+        d3.json(ifile, function(data) {
+            // console.log(data.features[0].geometry.coordinates);
+            var coords = data.features[0].geometry.coordinates;
+
+            var newcoords = [];
+            coords.forEach(function(d) {
+                var temp = [d[1], d[0]];
+                newcoords.push(temp);
+            });
+
+            var pathPattern = L.polylineDecorator(newcoords, {
+                patterns: [{
+                    offset: 0,
+                    repeat: 10,
+                    symbol: L.Symbol.dash({
+                        pixelSize: 5,
+                        pathOptions: {
+                            color: waycolor,
+                            weight: 3,
+                            opacity: 0.75
+                        }
+                    })
+                }, {
+                    offset: 25,
+                    repeat: 100,
+                    symbol: L.Symbol.arrowHead({
+                        pixelSize: 10,
+                        pathOptions: {
+                            color: waycolor,
+                            fillOpacity: 1,
+                            weight: 0
+                        }
+                    })
+                }]
+            }).addTo(layerobject);
+        }); // end d3
+    };
+
+    function loadRouteMulti(ifile, waycolor, layerobject) {
+        d3.json(ifile, function(data) {
+            // console.log(data.features[0].geometry.coordinates);
+            var coords = data.features[0].geometry.coordinates;
+
+            var newcoords = [];
+            coords.forEach(function(d) {
+                // console.log(d)
+                d.forEach(function(e) {
+                    // console.log(e);
+                    var temp = [e[1], e[0]];
+                    newcoords.push(temp);
+                })
+            })
+
+            var pathPattern = L.polylineDecorator(newcoords, {
+                patterns: [{
+                    offset: 0,
+                    repeat: 10,
+                    symbol: L.Symbol.dash({
+                        pixelSize: 5,
+                        pathOptions: {
+                            color: waycolor,
+                            weight: 3,
+                            opacity: 0.75
+                        }
+                    })
+                }, {
+                    offset: 25,
+                    repeat: 100,
+                    symbol: L.Symbol.arrowHead({
+                        pixelSize: 10,
+                        pathOptions: {
+                            color: waycolor,
+                            fillOpacity: 1,
+                            weight: 0
+                        }
+                    })
+                }]
+            }).addTo(layerobject);
+        }); // end d3
+    };
 
 
 
-        // })(); // end init()
+    // })(); // end init()
 
 
     // routing instructions
